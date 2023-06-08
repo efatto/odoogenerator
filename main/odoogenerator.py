ab7859a0005a49a4a676efe6f278d7d2f921a884
sergiocorato <sergiocorato@gmail.com>
2023-06-08 19:05:27 +0200

    [FIX] clean not used functions

--- main/odoogenerator.py
+++ main/odoogenerator.py
@@ -1,9 +1,6 @@
-import odoorpc
 import json
 import subprocess
-import time
 import os
-import signal
 from shutil import copy
 
 
@@ -46,19 +43,6 @@ class Connection:
         )
         self.pg_bin_path = '/usr/lib/postgresql/15/bin/'
         # todo get from system function
-        # if self.db_port in [
-        # '5439', '5440', '5441'] else ''
-
-    def odoo_connect(self):
-        self.client = odoorpc.ODOO(
-            host="localhost",
-            protocol="jsonrpc+ssl", port=self.options["http_port"]
-        )
-        self.client.login(
-            db=self.options["database"],
-            login=self.options["db_user"],
-            password=self.options["admin_passwd"],
-        )
 
     def _create_venv(self, branch=False):
         venv_path = self.venv_path
@@ -188,7 +172,6 @@ class Connection:
         process = subprocess.Popen(
             bash_command.split(), stdout=subprocess.PIPE, cwd=venv_path
         )
-        self.pid = process.pid
         if save_config:
             process.wait()
             subprocess.Popen(
@@ -213,167 +196,3 @@ class Connection:
                     ).wait()
         if update:
             process.wait()
-        # else:
-        #     time.sleep(15)
-            # self.odoo_connect()
-        # time.sleep(5)
-
-    def stop_odoo(self):
-        if self.pid:
-            os.kill(self.pid, signal.SIGTERM)
-            time.sleep(5)
-
-    def dump_database(self, version):
-        pg_bin_path = self.pg_bin_path
-        process = subprocess.Popen(
-            ['%spg_dump -O -p %s -d %s | gzip > %s/database.%s.gz' % (
-                 pg_bin_path, self.db_port, self.db, self.venv_path, version
-             )], shell=True)
-        process.wait()
-
-    def restore_filestore(self, from_version, to_version):
-        dump_file = os.path.join(self.path, 'filestore.tar')
-        if os.path.isfile(dump_file):
-            process = subprocess.Popen(
-                ['mv %s %s/filestore.%s.tar' % (
-                    dump_file, self.venv_path, from_version)], shell=True)
-            process.wait()
-        dump_file = os.path.join(
-            self.venv_path, 'filestore.%s.tar' % from_version)
-        filestore_path = '%s/openupgrade%s/data_dir/filestore' % (
-            self.venv_path, to_version)
-        if not os.path.isdir(filestore_path):
-            process = subprocess.Popen([
-                'mkdir -p %s' % filestore_path], shell=True)
-            process.wait()
-        process = subprocess.Popen([
-            'tar -zxvf %s -C %s/openupgrade%s/data_dir/filestore/' % (
-                dump_file, self.venv_path, to_version)], shell=True)
-        process.wait()
-
-    def dump_filestore(self, version):
-        process = subprocess.Popen([
-            'cd %s/openupgrade%s/data_dir/filestore && '
-            'tar -zcvf %s/filestore.%s.tar %s' % (
-                self.venv_path, version, self.venv_path, version, self.db)
-        ], shell=True)
-        process.wait()
-
-    def restore_db(self, from_version):
-        pg_bin_path = self.pg_bin_path
-        process = subprocess.Popen(
-            ['%sdropdb -p %s %s' % (pg_bin_path, self.db_port, self.db)],
-            shell=True)
-        process.wait()
-        process = subprocess.Popen(
-            ['%screatedb -p %s %s' % (
-                pg_bin_path, self.db_port, self.db)], shell=True)
-        process.wait()
-        dump_file = os.path.join(self.path, 'database.gz')
-        if os.path.isfile(dump_file):
-            process = subprocess.Popen(
-                ['mv %s %s/database.%s.gz' % (
-                    dump_file, self.venv_path, from_version)], shell=True)
-            process.wait()
-        dump_file = os.path.join(
-            self.venv_path, 'database.%s.gz' % from_version)
-
-        process = subprocess.Popen(
-            ['cat %s | gunzip | %spsql -U $USER -p %s -d %s ' % (
-                dump_file, pg_bin_path, self.db_port, self.db)], shell=True)
-        process.wait()
-
-    def auto_install_modules(self, version):
-        self.start_odoo(version)
-        module_obj = self.client.env['ir.module.module']
-        if version == '12.0':
-            self.remove_modules('upgrade')
-        receipt = self.receipts[version]
-        for modules in receipt:
-            module_list = modules.get('auto_install', False)
-            if module_list:
-                for module_pair in module_list:
-                    module_to_check = module_pair.split(' ')[0]
-                    module_to_install = module_pair.split(' ')[1]
-                    if module_obj.search([
-                            ('name', '=', module_to_check),
-                            ('state', '=', 'installed')]):
-                        self.client.env.install(module_to_install)
-        self.stop_odoo()
-
-    def uninstall_modules(self, version, before_migration=False, after_migration=False):
-        self.start_odoo(version)
-        if version == '12.0':
-            self.remove_modules('upgrade')
-        receipt = self.receipts[version]
-        for modules in receipt:
-            if after_migration:
-                modules_to_uninstall = modules.get('uninstall_after_migration', False)
-                if modules_to_uninstall:
-                    for module in modules_to_uninstall:
-                        self.install_uninstall_module(module, install=False)
-            if before_migration:
-                modules_to_uninstall = modules.get('uninstall_before_migration', False)
-                if modules_to_uninstall:
-                    for module in modules_to_uninstall:
-                        self.install_uninstall_module(module, install=False)
-        self.stop_odoo()
-
-    def delete_old_modules(self, version):
-        receipt = self.receipts[version]
-        if [modules.get('delete', False) for modules in receipt]:
-            self.start_odoo(version)
-            module_obj = self.client.env['ir.module.module']
-            for modules in receipt:
-                module_list = modules.get('delete', False)
-                if module_list:
-                    for module in module_list:
-                        module = module_obj.search([
-                            ('name', '=', module)])
-                        if module:
-                            module_obj.unlink(module.id)
-            self.stop_odoo()
-
-    def remove_modules(self, module_state=''):
-        if module_state == 'upgrade':
-            state = ['to upgrade', ]
-        else:
-            state = ['to remove', 'to install']
-        module_obj = self.client.env['ir.module.module']
-        modules = module_obj.search([('state', 'in', state)])
-        msg_modules = ''
-        msg_modules_after = ''
-        if modules:
-            msg_modules = str([x.name for x in modules])
-        for module in modules:
-            module.button_uninstall_cancel()
-        modules_after = module_obj.search(
-            [('state', '=', 'to upgrade')])
-        if modules_after:
-            msg_modules_after = str([x.name for x in modules_after])
-        print('Modules: %s' % msg_modules)
-        print('Modules after: %s' % msg_modules_after)
-
-    def install_uninstall_module(self, module, install=True):
-        module_obj = self.client.env['ir.module.module']
-        to_remove_modules = module_obj.search(
-            [('state', '=', 'to remove')])
-        for module_to_remove in to_remove_modules:
-            module_to_remove.button_uninstall_cancel()
-        state = self.client.env.modules(module)
-        if state:
-            if install:
-                self.client.env.install(module)
-            elif state.get('installed', False) or state.get('to upgrade', False)\
-                    or state.get('uninstallable'):
-                module_id = module_obj.search([('name', '=', module)])
-                if module_id:
-                    try:
-                        module_id.button_immediate_uninstall()
-                        print('Module %s uninstalled' % module)
-                        module_id.unlink()
-                    except Exception as e:
-                        print('Module %s not uninstalled for %s' % (module, e))
-                        pass
-                else:
-                    print('Module %s not found' % module)
